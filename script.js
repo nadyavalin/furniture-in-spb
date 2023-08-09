7e4da1a3b6c877efd34d09ace3b5e3f9d9f967e1
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-08-09 12:08:41 +0300

    feat: add adaptation, except sliders

--- script.js
+++ script.js
@@ -52,6 +52,29 @@ accordions.forEach((accordion) => {
 	});
 });
 
+// Бургер-меню
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector(".header");
+
+  document.getElementById("burger").addEventListener("click", function() {
+      setTimeout(() => {
+        header.classList.toggle("open");
+      }, 0)
+  })
+
+  document.addEventListener('keydown', (e) => {
+      if (e.key === "Escape") {
+          header.classList.remove("open");
+      }
+  });
+
+  document.body.addEventListener('click', event => {
+      if (!event.target.classList.contains("menu") && !event.target.closest(".burger") && header.classList.contains("open")) {        
+          header.classList.remove("open");
+      }
+  });
+})
+
 // Phone
 // пока не работает
 var cleave = new Cleave('form__tel', {
